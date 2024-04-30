bb5237f5289bb70ef574684eb2b7e31c27114607
kananis231 <kananisharvil603@gmail.com>
2024-04-30 18:46:11 +0530

    10

---
+++ myapp10/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Templeats from './Templeats';
+
+function App() {
+  return (
+    <>
+    <Templeats/>
+    </>
+  );
+}
+
+export default App;
---
+++ myapp10/src/Templeats.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Nav from './Comp/Nav'
+import Header from './Comp/Header'
+import Section1 from './Comp/Section1'
+import Section2 from './Comp/Section2'
+import Section3 from './Comp/Section3'
+import Footer from './Comp/Footer'
+function Templeats() {
+  return (
+    <>
+    <Nav/>
+    <Header/>
+    <Section1/>
+    <Section2/>
+    <Section3/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Templeats
