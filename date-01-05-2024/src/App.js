632af4bc66c5b282309f4d3165357108550886a9
kananis231 <kananisharvil603@gmail.com>
2024-05-01 18:24:56 +0530

    01-05-2024 git commit -m01-05-2024 git commit -m01-05-2024

---
+++ date-01-05-2024/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Templets from './Templets';
+
+function App() {
+  return (
+      <>
+      <Templets/>
+      </>
+  );
+}
+
+export default App;
---
+++ date-01-05-2024/src/Templets.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Nav from './Comp/Nav'
+import Heard from './Comp/Heard'
+import Section1 from './Comp/Section1'
+import Section2 from './Comp/Section2'
+import Section3 from './Comp/Section3'
+import Section4 from './Comp/Section4'
+import Section5 from './Comp/Section5'
+import Section6 from './Comp/Section6'
+import Footer from './Comp/Footer'
+
+function Templets() {
+  return (
+    <>
+    <Nav/>
+    <Heard/>
+    <Section1/>
+    <Section2/>
+    <Section3/>
+    <Section4/>
+    <Section5/>
+    <Section6/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Templets
