0aaa99ca3a6f9fd1e97ac9a501ee4b8b2f4c2e19
kananis231 <kananisharvil603@gmail.com>
2024-06-27 15:28:11 +0530

    update

--- firebase/src/Config.jsx
+++ firebase/src/Config.jsx
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAYkTSne_feiVND1RfUpA7Ze8SPLTA10hE",
-  authDomain: "test-89ccc.firebaseapp.com",
-  projectId: "test-89ccc",
-  storageBucket: "test-89ccc.appspot.com",
-  messagingSenderId: "75095675756",
-  appId: "1:75095675756:web:494ede0069add5f97fd949",
-  measurementId: "G-9J0J0ZMRNW"
+  apiKey: "AIzaSyBuZA7WGq9ZmMrUJ1_sldd_1485g0uhq4E",
+  authDomain: "auth-a73d9.firebaseapp.com",
+  projectId: "auth-a73d9",
+  storageBucket: "auth-a73d9.appspot.com",
+  messagingSenderId: "208919388729",
+  appId: "1:208919388729:web:98597e33ebe8fa917a63e8",
+  measurementId: "G-7T463MBCD9"
 };
 
 
